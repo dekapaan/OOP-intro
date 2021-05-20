010458720374b10a6ebbda2aa431c2ffe1ca67a0
dekapaan <54145779+dekapaan@users.noreply.github.com>
2021-05-20 13:08:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,40 +22,40 @@ class Shapes(object):
         self.name = name
         self.sides = sides
 
-#
-# class Circle(Shapes):
-#     def __init__(self, radius):
-#         self.radius = radius
-#
-#     def area(self):
-#         circle_area = pi*(self.radius**2)
-#         return circle_area
-#
-#
-# class Triangle(Shapes):
-#     def __init__(self, base, height):
-#         self.base = base
-#         self.height = height
-#
-#     def area(self):
-#         triangle_area = 0.5*self.base*self.height
-#         return triangle_area
-#
-#
-# class Rectangle(Shapes):
-#     def __init__(self, length, width):
-#         self.width = width
-#         self.length = length
-#
-#     def area(self):
-#         rectangle_area = self.width*self.length
-#         return rectangle_area
-#
-#
-# class Square(Shapes):
-#     def __init__(self, side):
-#         self.side = side
-#
-#     def area(self):
-#         square_area = self.side**2
-#         return square_area
+
+class Circle(Shapes):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        circle_area = pi*(self.radius**2)
+        return circle_area
+
+
+class Triangle(Shapes):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        triangle_area = 0.5*self.base*self.height
+        return triangle_area
+
+
+class Rectangle(Shapes):
+    def __init__(self, length, width):
+        self.width = width
+        self.length = length
+
+    def area(self):
+        rectangle_area = self.width*self.length
+        return rectangle_area
+
+
+class Square(Shapes):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        square_area = self.side**2
+        return square_area
